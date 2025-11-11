e1015928c95dc4712f60b46524dae165559e8743
Adam Rangwala <adamrangwala@wustl.edu>
2025-11-11 12:25:30 -0600

    Render Bug: Critical fix - AutHRequest extend with request

--- act-math-mern-app/server/controllers/authController.ts
+++ act-math-mern-app/server/controllers/authController.ts
@@ -1,4 +1,4 @@
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { auth, db } from '../config/firebase';
 import { DecodedIdToken } from 'firebase-admin/auth';
 
